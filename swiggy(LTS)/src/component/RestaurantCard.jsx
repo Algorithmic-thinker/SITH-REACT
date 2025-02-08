9ec9f9068005bbddadbffa32a34bdb71b667f698
Algorithmic-thinker <piyushmeshram2@gmail.com>
2025-02-08 19:20:33 +0530

    delivery time added

--- swiggy(LTS)/src/component/RestaurantCard.jsx
+++ swiggy(LTS)/src/component/RestaurantCard.jsx
@@ -10,7 +10,7 @@ const RestaurantCard = ({name, rating, deliveryTIme, cuisines, location}) =>{
                 <h2 className="font-semibold text-2xl">{name}</h2>
                 <div className="flex gap-2">
                     <h2><i className="fa-solid fa-star text-[16px] text-green-600"></i>{rating}</h2>
-                    <h2 className="text-[16px] ">{deliveryTIme}</h2>
+                    <h2 className="text-[16px] ">{deliveryTime}</h2>
                 </div>
             </div>
             <p className="text-gray-500 px-3">{cuisines}</p>
